df3256286d0628b59b2818c8cadc9b6ef3f1de15
hsonneborn-dev <hsonneborn@ideo.edu.ar>
2025-09-01 15:27:30 -0300

    trabajo heimdall camilo

--- index.php
+++ index.php
@@ -1,14 +1,21 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="stilos.css">
 </head>
+
 <body>
-    <a href="julio cesar.php">julio cesar</a>
-  <h1>holoa en este lugar veremos a grandes generales de la historia</h1>
- <h3>oidremos ver tanto a julio cesar como napoleon. a annibal y a genghis </h3>
-    
+    <div class="contenedor1">
+        <a href="napoleon.php">napoleon bonaparte</a>
+        <a href="julio cesar.php">julio cesar</a>
+    </div>
+    <h1>holoa en este lugar veremos a grandes generales de la historia</h1>
+    <h3>oidremos ver tanto a julio cesar como napoleon. a annibal y a genghis </h3>
+
 </body>
+
 </html>
---
+++ napoleon.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+     <link rel="stylesheet" href="stilos.css">
+</head>
+<body><div class="contenedor3">
+    <a href="index.php"> inicio</a>
+<a href="julio cesar.php">julio cesar</a>
+</div>
+    <img src="imagenes/17490.jpg" alt="">
+<h1>napoleon fuen un general consagrado y despues un emperador bienamado por el pueblo frances
+su fama crecio con hazañas como la batalla de austerlitz.
+napoleon nacio en corcega  15 de agosto de 1769 participo en la revolucion francesa
+se comvirtio en el primer consul de la republica francesa y posteriormente en el emperador frances
+aunque despues sufrio derrotas desastrozas como la batalla maritima de trafalgar la batalla de waterloo y la invasion a rusia
+</h1>    
+</body>
+</html>
