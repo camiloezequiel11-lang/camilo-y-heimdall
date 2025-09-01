fd46f1d3fc89c45b85de9913bd11923b7e996af6
hsonneborn-dev <hsonneborn@ideo.edu.ar>
2025-09-01 13:42:56 -0300

    trabajo camilo heimdall

--- index.php
+++ index.php
@@ -6,10 +6,9 @@
     <title>Document</title>
 </head>
 <body>
-    <h1>Heindall</h1>
- 
-    <h1>hola camilo</h1>
- 
+    <a href="julio cesar.php">julio cesar</a>
+  <h1>holoa en este lugar veremos a grandes generales de la historia</h1>
+ <h3>oidremos ver tanto a julio cesar como napoleon. a annibal y a genghis </h3>
     
 </body>
 </html>
